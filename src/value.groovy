789bf49fea1c0f987631d0fea8ebcff9d98f6d05
mathanika <43029671+mathanika@users.noreply.github.com>
2021-01-27 22:20:03 +0530

    creating another class file

---
+++ src/value.groovy
@@ -0,0 +1,12 @@
+#!/usr/bin/env groovy
+package com.cleverbuilder
+
+class GlobalVars {
+   static String foo = "bar"
+   static string welcome = "Hi, welcome"
+
+   // refer to this in a pipeline using:
+   //
+   // import com.cleverbuilder.GlobalVars
+   // println GlobalVars.foo
+}
