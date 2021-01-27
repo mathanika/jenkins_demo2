828e4d83b987731de58545a16bfd2d888f017dd2
mathanika <43029671+mathanika@users.noreply.github.com>
2021-01-27 22:26:58 +0530

    Update value.groovy

--- src/value.groovy
+++ src/value.groovy
@@ -3,7 +3,7 @@ package com.cleverbuilder
 
 class GlobalVars {
    static String foo = "bar";
-   static string welcome = "Hi, welcome";
+   //static string welcome = "Hi, welcome";
 
    // refer to this in a pipeline using:
    //
